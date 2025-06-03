789812199611d95aff213407b25bdc2795c9e10e
Benedikt Magnus <magnus@magnuscraft.de>
2025-06-03 15:10:53 +0200

    Andikopf mod

---
+++ AndikopfMod/andikopf.mod
@@ -0,0 +1,8 @@
+{
+    "name": "Andikopf",
+    "description": "Der Entwickler im Spiel!",
+    "author": "Benedikt Magnus",
+    "version": "0.1.0",
+    "supported_game_version": "0.5.266",
+    "supported_mod_loader_version": "0.1.0"
+}
---
+++ AndikopfMod/mods/andikopf/mod.gd
@@ -0,0 +1,17 @@
+extends Node
+
+func _enter_tree ():
+    get_parent().on_scene_loaded.connect(_on_scene_loaded)
+
+func _exit_tree ():
+    get_parent().on_scene_loaded.disconnect(_on_scene_loaded)
+
+func _on_scene_loaded (scene_root: Node):
+    print(scene_root.scene_file_path)
+    if scene_root.scene_file_path == "res://Characters/dwarf/Puppet_Gnome.tscn":
+        _on_gnome_loaded(scene_root)
+
+func _on_gnome_loaded (gnome_scene: Node2D):
+    gnome_scene.get_node("BodyParts/HeadAnchor/HeadNormal/EyeBase").visible = false
+    gnome_scene.get_node("BodyParts/HeadAnchor/HeadConfused/EyeBase").visible = false
+    gnome_scene.get_node("BodyParts/HeadAnchor/HeadSus/EyeBase").visible = false
